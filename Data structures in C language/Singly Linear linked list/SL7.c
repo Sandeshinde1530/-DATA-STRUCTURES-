788114ca4a5dd08f034ff3ab94bb04207b9a4207
Sandesh <sandesh30apirl@gmail.com>
2025-05-24 16:29:07 +0530

    24may

---
+++ Data structures in C language/Singly Linear linked list/SL7.c
@@ -0,0 +1,234 @@
+// completed this code in just 21.56  minutes
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE head , int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*head == NULL)
+    {
+        *head = newn;
+    }
+    else
+    {
+        newn->next = *head;
+        *head = newn;
+    }
+}
+
+void InsertLast(PPNODE head , int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = no;
+    newn->next = NULL;
+
+    if(*head == NULL)
+    {
+        *head = newn;
+    }
+    else
+    {
+        temp = *head;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+    }
+
+}
+
+void DeleteFirst(PPNODE head)
+{
+    if(*head == NULL)
+    {
+        return;
+    }
+    else if((*head)->next == NULL)
+    {
+        free(*head);
+        *head = NULL;
+    }
+    else
+    {
+        PNODE temp = *head;
+        *head = temp->next;
+        free(temp);
+    }
+
+}
+void DeleteLast(PPNODE head)
+{
+     if(*head == NULL)
+    {
+        return;
+    }
+    else if((*head)->next == NULL)
+    {
+        free(*head);
+        *head = NULL;
+    }
+    else
+    {
+        PNODE temp = *head;
+       
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+
+}
+
+void Display(PNODE head)
+{
+    while(head != NULL)
+    {
+        printf("| %d |->",head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+
+}
+
+int Count(PNODE head)
+{
+    int iCount = 0;
+     while(head != NULL)
+    {
+        iCount++;
+        head = head->next;
+    }
+
+    return iCount;
+}
+
+void InsertAtPos(PPNODE head , int no , int iPos)
+{
+    int iCount = Count(*head);
+
+    if(iPos < 1 || iPos > iCount + 1)
+    {
+        printf("Invalid position\n");
+        return;
+    }
+    else if(iPos == 1)
+    {
+        InsertFirst(head , no);
+    }
+    else if(iPos == iCount +1)
+    {
+        InsertLast(head , no);
+    }
+    else
+    {
+        PNODE newn = NULL; 
+        PNODE temp = *head; 
+
+        newn = (PNODE)malloc(sizeof(NODE));
+        newn->data = no;
+        newn->next = NULL;
+
+        for(int i = 1 ; i <iPos -1 ;i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+
+void DeleteAtPos(PPNODE head ,int iPos)
+{
+    int iCount = Count(*head);
+
+    if(iPos < 1 || iPos > iCount)
+    {
+        printf("Invalid position\n");
+        return;
+    }
+    else if(iPos == 1)
+    {
+        DeleteFirst(head );
+    }
+    else if(iPos == iCount +1)
+    {
+        DeleteLast(head );
+    }
+    else
+    {
+        PNODE temp = *head; 
+        PNODE target = NULL; 
+
+        for(int i = 1 ; i <iPos -1 ;i++)
+        {
+            temp = temp->next;
+        }
+        target = temp->next;
+        temp->next = target->next;
+    }
+}
+
+int main()
+{
+    PNODE first = NULL;
+    int iRet = 0;
+
+    InsertFirst(&first , 51);
+    InsertFirst(&first , 21);
+    InsertFirst(&first , 11);
+    Display(first);
+    iRet = Count(first);
+    printf("NO. of nodes are: %d\n",iRet);
+    
+    
+    InsertLast(&first , 101);
+    InsertLast(&first , 111);
+    InsertLast(&first , 121);
+    Display(first);
+    iRet = Count(first);
+    printf("NO. of nodes are: %d\n",iRet);
+    
+    // DeleteFirst(&first);
+    // Display(first);
+    // iRet = Count(first);
+    // printf("NO. of nodes are: %d\n",iRet);
+    
+    // DeleteLast(&first);
+    // Display(first);
+    // iRet = Count(first);
+    // printf("NO. of nodes are: %d\n",iRet);
+    
+    InsertAtPos(&first , 75 , 4);
+    Display(first);
+    iRet = Count(first);
+    printf("NO. of nodes are: %d\n",iRet);
+    
+    DeleteAtPos(&first , 4);
+    Display(first);
+    iRet = Count(first);
+    printf("NO. of nodes are: %d\n",iRet);
+
+
+    return 0;
+}
---
+++ Data structures in C++language/DoublyLinear linked list/Dl1.cpp
@@ -0,0 +1,260 @@
+#include<iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+
+class DoublyLL
+{
+    public:
+        PNODE head;
+        int iCount;
+
+        DoublyLL()
+        {
+            head = NULL;
+            iCount = 0;
+        }
+
+        void InsertFirst(int no)
+        {
+            PNODE newn = NULL;
+
+            newn = new NODE;
+            newn->data = no;
+            newn->prev= NULL;
+            newn->next= NULL;
+            
+            if(head == NULL)
+            {
+                head = newn;
+            }
+            else
+            {
+                head->prev = newn;
+                newn->next = head;
+                head = newn;
+            }
+            iCount++;
+            
+        }
+
+        void InsertLast(int no)
+        {
+            PNODE newn = NULL;
+            PNODE temp = NULL;
+
+            newn = new NODE;
+            newn->data = no;
+            newn->prev= NULL;
+            newn->next= NULL;
+            
+            if(head == NULL)
+            {
+                head = newn;
+            }
+            else
+            {
+               temp = head;
+
+               while(temp->next != NULL)
+               {
+                temp = temp->next;
+               }
+               newn->prev = temp;
+               temp->next = newn;
+            }
+            iCount++;
+        }
+        
+        void InsertAtPos(int no , int iPos)
+        {
+            if(iPos < 0 || iPos > iCount + 1)
+            {
+                cout<<"Invalid POsition\n";
+                return;
+            }
+            else if(iPos == 1)
+            {
+                InsertFirst(no);
+            }
+            else if(iPos == iCount +1)
+            {
+                InsertLast(no);
+            }
+            else
+            {
+                PNODE newn = NULL;
+                PNODE temp = head;
+
+                newn = new NODE;
+                newn->data = no;
+                newn->prev = NULL;
+                newn->next = NULL;
+
+                int i = 0;
+                for(i = 1 ; i<iPos-1 ; i++)
+                {
+                    temp = temp->next;
+                }
+                newn->next = temp->next;
+                temp->next->prev = newn;
+
+                temp->next = newn;
+                newn->prev = temp;
+            }
+            iCount++;
+
+        }
+        
+        void DeleteFirst()
+        {
+            PNODE temp = NULL;
+
+            if(head == NULL)
+            {
+                return;
+            }
+            else if(head->next == NULL)
+            {
+                delete head;
+                head= NULL;
+            }
+            else
+            {
+                temp = head;
+
+                head = temp->next;
+                head->prev = NULL;
+                delete temp;
+            }
+            iCount--;
+        }
+        
+        void DeleteLast()
+        {
+            PNODE temp = head;
+
+            if(head == NULL)
+            {
+                return;
+            }
+            else if(head ->next == NULL)
+            {
+                delete(head);
+                head= NULL;
+            }
+            else
+            {
+                temp = head;
+
+                while(temp ->next->next != NULL)
+                {
+                    temp = temp->next;
+                }
+                delete temp->next;
+                temp->next = NULL;
+            }
+            iCount--;
+        }
+
+        void DeleteAtPos(int iPos)
+        {
+             if(iPos < 0 || iPos > iCount)
+            {
+                cout<<"Invalid POsition\n";
+                return;
+            }
+            else if(iPos == 1)
+            {
+                DeleteFirst();
+            }
+            else if(iPos == iCount)
+            {
+                DeleteLast();
+            }
+            else
+            {
+                PNODE temp = head;
+                PNODE target = NULL;
+               
+                int i = 0;
+                for(i = 1 ; i<iPos-1 ; i++)
+                {
+                    temp = temp->next;
+                }
+                target = temp->next;
+
+                temp->next = target->next;
+                temp->next->prev = temp;
+                delete target;
+                
+            }
+            iCount--;
+
+        }
+
+        void Display()
+        {
+            PNODE temp = head;
+            while(temp != NULL)
+            {
+                cout<<"| "<<temp->data<<" |->";
+                temp = temp->next;
+            }
+            cout<<"NULL\n";
+        }
+
+        int Count()
+        {
+            return iCount;
+        }
+
+};
+
+int main()
+{
+    DoublyLL dobj;
+    int iRet = 0;
+
+    dobj.InsertFirst(3);
+    dobj.InsertFirst(2);
+    dobj.InsertFirst(1);
+    dobj.Display();
+    iRet = dobj.Count();
+    cout<<"NO. of nodes are :"<<iRet<<"\n";
+    
+    dobj.InsertLast(4);
+    dobj.InsertLast(5);
+    dobj.InsertLast(6);
+    dobj.Display();
+    iRet = dobj.Count();
+    cout<<"NO. of nodes are :"<<iRet<<"\n";
+    
+    // dobj.DeleteFirst();
+    // dobj.Display();
+    
+    // dobj.DeleteLast();
+    // dobj.Display();
+    
+    dobj.InsertAtPos(35 , 4);
+    dobj.Display();
+    iRet = dobj.Count();
+    cout<<"NO. of nodes are :"<<iRet<<"\n";
+    
+    dobj.DeleteAtPos(4);
+    dobj.Display();
+    iRet = dobj.Count();
+    cout<<"NO. of nodes are :"<<iRet<<"\n";
+
+    return 0;
+}
