acfb9215f328193710295116d66d0fddb843d5db
Sandesh <sandesh30apirl@gmail.com>
2025-07-03 00:16:01 +0530

    practice

--- Data structures in C++language/DoublyLinear linked list/DL6.cpp
+++ Data structures in C++language/DoublyLinear linked list/DL6.cpp
@@ -2,23 +2,27 @@
 
 using namespace std;
 
+
+template <class T>
 struct node
 {
-    int data;
-    struct node *prev;
-    struct node *next;
+    T data;
+    struct node<T> *prev;
+    struct node<T> *next;
 };
 
+
+template <class T>
 class DoublyLinear
 {
     public:
         int iCount;
-        struct node *head ;
+        struct node<T> *head ;
 
         DoublyLinear();
-        void InsertFirst(int no);
-        void InsertLast(int no);
-        void InsertAtPos(int no , int iPos);
+        void InsertFirst(T no);
+        void InsertLast(T no);
+        void InsertAtPos(T no , int iPos);
         void DeleteFirst();
         void DeleteLast();
         void DeleteAtPos(int iPos);
@@ -27,16 +31,18 @@ class DoublyLinear
 };
 
 
-DoublyLinear ::  DoublyLinear()
+template <class T>
+DoublyLinear<T> ::  DoublyLinear()
 {
     iCount = 0;
     head = NULL;    
 }
 
-void DoublyLinear :: InsertFirst(int no)
+template <class T>
+void DoublyLinear<T> :: InsertFirst(T no)
 {
-    struct node *newn = NULL;
-    newn = new node;
+    struct node<T> *newn = NULL;
+    newn = new node<T>;
 
     newn->data = no;
     newn->prev = NULL;
@@ -55,11 +61,12 @@ void DoublyLinear :: InsertFirst(int no)
     iCount++;
 }
 
-void DoublyLinear :: InsertLast(int no)
+template <class T>
+void DoublyLinear<T> :: InsertLast(T no)
 {
-    struct node *temp = head;
-    struct node *newn = NULL;
-    newn = new node;
+    struct node<T> *temp = head;
+    struct node<T> *newn = NULL;
+    newn = new node<T>;
 
     newn->data = no;
     newn->prev = NULL;
@@ -73,14 +80,16 @@ void DoublyLinear :: InsertLast(int no)
     {
        while(temp -> next != NULL)
        {
+            temp = temp->next;
+        }   
         temp->next = newn;
         newn->prev = temp;  
-       }
     }  
     iCount++;
 }
 
-void DoublyLinear :: InsertAtPos(int no , int iPos)
+template <class T>
+void DoublyLinear<T> :: InsertAtPos(T no , int iPos)
 {
     if(iPos <1 ||  iPos > iCount + 1)
     {
@@ -97,10 +106,10 @@ void DoublyLinear :: InsertAtPos(int no , int iPos)
     }
     else
     {
-        struct node *temp = head;
-        struct node *newn = NULL;
+        struct node<T> *temp = head;
+        struct node<T> *newn = NULL;
 
-        newn = new node;
+        newn = new node<T>;
         newn->data = no;
         newn->next = NULL;
         newn->prev = NULL;
@@ -111,19 +120,20 @@ void DoublyLinear :: InsertAtPos(int no , int iPos)
         }
         newn->next = temp->next;
         temp->next->prev = newn;
-        newn->prev = NULL;
-        newn->next = NULL;
+        newn->prev = temp;
+        temp->next = newn;
     }
     iCount++;    
 }
 
-void DoublyLinear :: DeleteFirst()
+template <class T>
+void DoublyLinear<T> :: DeleteFirst()
 {
      if(head == NULL)
      {
         return;
      }
-     else if(head ->next = NULL)
+     else if(head ->next == NULL)
      {
         delete head;
         head == NULL;
@@ -131,40 +141,42 @@ void DoublyLinear :: DeleteFirst()
      else
      {
         head = head->next;
-        delete head;
-        head == NULL;
+        delete head->prev;
+        head->prev =  NULL;
      }
-     iCount;
+     iCount--;
 }
 
-void DoublyLinear :: DeleteLast()
+template <class T>
+void DoublyLinear<T> :: DeleteLast()
 {
-    struct node *temp = head;
+    struct node<T> *temp = head;
 
     if(head == NULL)
      {
         return;
      }
-     else if(head ->next = NULL)
+     else if(head ->next ==  NULL)
      {
         delete head;
         head == NULL;
      }
      else
      {
-        while(temp->next->next == NULL)
+        while(temp->next->next != NULL)
         {
             temp = temp->next;
         }
         delete temp->next;
         temp->next = NULL;
      }
-     iCount--
+     iCount--;
 }
 
-void DoublyLinear :: DeleteAtPos(int iPos)
+template <class T>
+void DoublyLinear<T> :: DeleteAtPos(int iPos)
 {
-        if(iPos <1 ||  iPos > iCount + 1)
+        if(iPos <1 ||  iPos > iCount)
     {
         cout<<"Invalid Syntax\n";
         return ;
@@ -179,8 +191,8 @@ void DoublyLinear :: DeleteAtPos(int iPos)
     }
     else
     {
-        struct node *temp = head;
-        struct node *target = NULL;
+        struct node<T> *temp = head;
+        struct node<T> *target = NULL;
 
         for (int i = 1 ; i < iPos - 1 ; i++)
         {
@@ -195,25 +207,64 @@ void DoublyLinear :: DeleteAtPos(int iPos)
     iCount--;    
 }
 
-void DoublyLinear :: Display()
+template <class T>
+void DoublyLinear<T> :: Display()
 {
-        struct node * temp = head;
+        struct node<T> * temp = head;
 
         while(temp != NULL)
         {
             cout<<"| "<<temp->data<<" | <=>";
             temp = temp->next;
         }
+        cout<<endl;
 }
 
 
-int Count()
+template <class T>
+int DoublyLinear<T> :: Count()
 {
     return iCount;
 }
 
 int main()
 {
+    DoublyLinear <float>dobj;
+    int iRet = 0;
+
+    dobj.InsertFirst(51.054f);
+    dobj.InsertFirst(21.054f);
+    dobj.InsertFirst(11.054f);
+    iRet = dobj.Count();
+    cout<<"Count is :"<<iRet<<endl;
+    dobj.Display();
+
+    dobj.InsertLast(101.054f);
+    dobj.InsertLast(111.054f);
+    dobj.InsertLast(121.054f);
+    iRet = dobj.Count();
+    cout<<"Count is :"<<iRet<<endl;
+    dobj.Display();
+
+    dobj.InsertAtPos(75.054f , 4);
+    iRet = dobj.Count();
+    cout<<"Count is :"<<iRet<<endl;
+    dobj.Display();
+
+    dobj.DeleteAtPos(4);
+    iRet = dobj.Count();
+    cout<<"Count is :"<<iRet<<endl;
+    dobj.Display();
+
+    dobj.DeleteFirst();
+    iRet = dobj.Count();
+    cout<<"Count is :"<<iRet<<endl;
+    dobj.Display();
+
+    dobj.DeleteLast();
+    iRet = dobj.Count();
+    cout<<"Count is :"<<iRet<<endl;
+    dobj.Display();
 
 
     return 0;
